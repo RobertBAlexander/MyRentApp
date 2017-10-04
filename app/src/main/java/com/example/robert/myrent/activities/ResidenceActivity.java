781472a553fd5ca108bdebdcc590bcc1c13ab4d8
Robert Alexander <robert.b.alexander@gmail.com>
2017-10-04 09:38:58 +0100

    Secion 5 of lab 3a. Introduced new functions and fixed some old ones.

---
+++ app/src/main/java/com/example/robert/myrent/activities/ResidenceActivity.java
@@ -0,0 +1,44 @@
+package com.example.robert.myrent.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.example.robert.myrent.R;
+import com.example.robert.myrent.models.Residence;
+
+public class ResidenceActivity extends AppCompatActivity implements TextWatcher {
+
+    private EditText geolocation;
+    private Residence residence;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_residence);
+
+        geolocation = (EditText) findViewById(R.id.geolocation);
+        residence = new Residence();
+
+        // Register a TextWatcher in the EditText geolocation object
+        geolocation.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        residence.setGeolocation(editable.toString());
+
+    }
+}
